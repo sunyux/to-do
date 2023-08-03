369d4d8cea5dffde9221f0178ed0aced198ccf27
sunyux <74965440+sunyux@users.noreply.github.com>
2023-08-03 23:33:39 +0800

    add note with new item table

--- todo/database/migrations/2023_08_03_081108_create_items_table.php
+++ todo/database/migrations/2023_08_03_081108_create_items_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->boolean('completed')->default(flase);
+            $table->timestamp('completed_at')->nullable();
             $table->timestamps();
         });
     }
